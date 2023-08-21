3efffbf64d26b2089ff3bda0607e637696083e88
polin <polina.suvorova@gmail.com>
2023-08-21 18:23:09 +0300

    optimize import

--- src/main/java/org/graduation/restaurantvoting/config/OpenApiConfig.java
+++ src/main/java/org/graduation/restaurantvoting/config/OpenApiConfig.java
@@ -28,7 +28,7 @@ import org.springframework.context.annotation.Configuration;
                         - admin@gmail.com / admin<br>
                         - guest@gmail.com / guest</p>
                         """,
-                contact = @Contact(url = "https://github.com/PolinaSuvorova" , name = "Polina Suvorova", email = "polina.tatara@gmail.com")
+                contact = @Contact(url = "https://github.com/PolinaSuvorova", name = "Polina Suvorova", email = "polina.tatara@gmail.com")
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
