1cb882d3de4497cd3c2109a40d88ecb47629e464
polin <polina.suvorova@gmail.com>
2023-08-21 18:47:18 +0300

    optimize import

--- src/main/java/org/graduation/restaurantvoting/RestaurantVotingApplication.java
+++ src/main/java/org/graduation/restaurantvoting/RestaurantVotingApplication.java
@@ -1,14 +1,10 @@
 package org.graduation.restaurantvoting;
 
-import lombok.AllArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@AllArgsConstructor
-public class RestaurantVotingApplication { //implements ApplicationRunner {
+public class RestaurantVotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
