ea7c2098865cb42fd1634023642b599f113c70a9
polin <polina.suvorova@gmail.com>
2023-08-21 17:56:24 +0300

    add description for api

--- src/main/java/org/graduation/restaurantvoting/util/VoteUtils.java
+++ src/main/java/org/graduation/restaurantvoting/util/VoteUtils.java
@@ -1,12 +1,13 @@
 package org.graduation.restaurantvoting.util;
 
+import lombok.experimental.UtilityClass;
 import org.graduation.restaurantvoting.model.Vote;
 import org.graduation.restaurantvoting.to.VoteTo;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+@UtilityClass
 public class VoteUtils {
 
     public static VoteTo convertTo(Vote vote) {
